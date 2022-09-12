41ff6e148e07bc53fb5ce181bac475ab8f4d6a86
David <4292@holbertonschool.com>
2022-09-12 19:59:20 -0400

    trying to find the invisible checker bug by making my code worse

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -20,8 +20,7 @@ if __name__ == "__main__":
         response2 = requests.get('https://jsonplaceholder.typicode.com/todos')
         task_list = json.loads(response2.text)
 
-        """ Gets total number of tasks related to user,
-        amount of those tasks that are done, and the names of those tasks """
+        """ Exports formatted info to CSV format """
         with open('{}.csv'.format(user['id']), 'w', encoding='utf-8') as f:
             writer = csv.writer(f, quoting=csv.QUOTE_ALL)
             for task in task_list:
@@ -32,3 +31,21 @@ if __name__ == "__main__":
                         task['completed'],
                         task['title']
                     ])
+
+        """ Gets total number of tasks related to user,
+        amount of those tasks that are done, and the names of those tasks """
+        total_tasks = 0
+        tasks_done = 0
+        task_names = []
+        for task in task_list:
+            if user['id'] == task['userId']:
+                total_tasks += 1
+                if task['completed'] is True:
+                    task_names.append(task['title'])
+                    tasks_done += 1
+
+        """ Prints all information gathered in a specific format """
+        print('Employee {} is done with tasks({}/{}):'
+              .format(user['name'], tasks_done, total_tasks))
+        for title in task_names:
+            print('\t {}'.format(title))
