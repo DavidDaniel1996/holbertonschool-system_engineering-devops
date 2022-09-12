bacd348d99bddbf5721121799d7f78f7130c3ff3
David <4292@holbertonschool.com>
2022-09-12 19:36:55 -0400

    CSV export

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Module using REST API that returns info of to-do list
+    and records it in CSV Format """
+
+import csv
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    if sys.argv[1].isdigit() is True:
+        match_id = int(sys.argv[1])
+
+        """ Gets user corresponding to id passed as command line argument """
+        response1 = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(match_id))
+        user = json.loads(response1.text)
+
+        """ Gets list of all tasks available in the to-do list """
+        response2 = requests.get('https://jsonplaceholder.typicode.com/todos')
+        task_list = json.loads(response2.text)
+
+        """ Gets total number of tasks related to user,
+        amount of those tasks that are done, and the names of those tasks """
+        with open('{}.csv'.format(user['id']), 'w', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            for task in task_list:
+                if user['id'] == task['userId']:
+                    writer.writerow([
+                        user['id'],
+                        user['username'],
+                        task['completed'],
+                        task['title']
+                    ])
